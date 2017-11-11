026538b86b88fc4e0135b3d0ecaf327832e32431
alikhalid <ali.kahn92@gmail.com>
2017-11-10 19:26:45 -0600

    Developing cache...

--- newImpl/common/cache/Cache.h
+++ newImpl/common/cache/Cache.h
@@ -1,41 +1,41 @@
-#include<string>
-#include<map>
-#include<utility>
+#include <string>
+#include <map>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
-#define PSS pair<string, string>
-
 class Cache {
     private:
-        map<PSS, int> data;
+        map<string, map<string, int>> data;
 
     public:
         Cache(){ }
         ~Cache(){ }
 
-        //Inserts the overlap value for a to b into the cache
+        //Inserts the strings with strings of all possible overlap
         void put(const string& a, const string& b, const int& value) {
-            PSS pss = make_pair(a, b);
-            data[pss] = value;
+            data[a][b] = value;
         }
 
         //Returns the cached value for a to b.
         //If the overlap value is not in the cache, it returns -1
         int get(const string& a, const string& b) {
-            PSS pss = make_pair(a, b);
-            int value = -1;
-            if(data.find(pss) != data.end()){
-                value = data[pss];
-            }
-
-            return value;
+            return data[a][b];
         }
 
         //Erases the overlap data for string a and b in any order (a to b AND b to a)
         void remove(const string& a, const string& b){
             data.erase(make_pair(a, b));
             data.erase(make_pair(b, a));
+        }
+
+        void insertNewOverlap(string& a, string& b, string& ab)
+        {
+          data.erase(a);
+          data[b].erase(a);
+          map<string, int> abMap(data[b]);
+          data[ab] = abMap;
         }
 
 };
