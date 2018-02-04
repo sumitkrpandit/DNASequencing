4fd5827050ee330b695fc7e3f285bd469c83810f
alikhalid <ali.kahn92@gmail.com>
2018-02-04 16:23:28 -0600

    Adding improved cache that uses map of vector.

--- newImpl/common/cache/Cache.h
+++ newImpl/common/cache/Cache.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class Cache {
   private:
-    map<string, map<string, int>> data;
+    map<string, vector<pair<string, int>>> data;
 
   public:
     Cache(){ }
@@ -15,25 +15,30 @@ class Cache {
 
     //Inserts the strings with strings of all possible overlap
     void put(const string& a, const string& b, const int& value) {
-      data[a][b] = value;
+      data[a].push_back(make_pair(b, value));
     }
 
     //Returns the cached value for a to b.
     //If the overlap value is not in the cache, it returns -1
-    int get(const string& a, const string& b) {
-      return data[a][b];
+    pair<string, int> get(const string& a) {
+      return data[a].front();
     }
 
     //Erases the overlap data for string a
     void remove(const string& a, const string& b){
-      data.erase(a);
-      data[b].erase(a);
     }
 
     void insertNewOverlap(string& a, string& b, string& ab) {
-      remove(a, b);
-      map<string, int> abMap(data[b]);
-      data[ab] = abMap;
-    }
+      auto it = data.find(b);
+      it.first = ab;
+
+      for (auto & entry : data) {
+        if (entry.first != ab) {
+          for (auto & v : entry.second) {
+            if (v.first == a)
+              v.first = ab;
+          }
+        }
+      }
 
 };
